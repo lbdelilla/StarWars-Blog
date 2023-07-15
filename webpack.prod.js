3d921211ac48e93a2eb7f6f15b9b719c5e4c0da2
Lucia Belen <lbdelilla@gmail.com>
2023-07-15 18:12:38 +0200

    fix: change import in webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,15 +1,15 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
-const Dotenv = require('dotenv-webpack');
-module.exports = merge(common, {
-    mode: 'production',
-    output: {
-        publicPath: './'
-    },
-    plugins: [
-        new Dotenv({
-            safe: true,
-            systemvars: true
-        })
-    ]
-});
+import { merge } from 'webpack-merge'
+import common from './webpack.common.js'
+import Dotenv from 'dotenv-webpack'
+export default merge(common, {
+  mode: 'production',
+  output: {
+    publicPath: './',
+  },
+  plugins: [
+    new Dotenv({
+      safe: true,
+      systemvars: true,
+    }),
+  ],
+})
